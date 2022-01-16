76b7de6206fbe01564a52f5427cb795ee350c534
Logan <loggeek.top@gmail.com>
2022-01-16 12:58:20 +0100

    Added comments + Cleanup

--- src/modules/board/Board.java
+++ src/modules/board/Board.java
@@ -8,9 +8,10 @@ package modules.board;
 
 public class Board
 {
-	byte[][] board1; // no rotation
-	byte[][] board2; // flipped 90° right
-	byte[][] board3; // flipped 90° left
+	byte[][] board1; // No rotation
+	byte[][] board2; // Flipped 90° right
+	byte[][] board3; // Flipped 90° left
+	int playerNum;
 	
 	/**
 	 * Creates a new board.
@@ -26,13 +27,15 @@ public class Board
 	
 	public Board(int playerNum) throws IllegalArgumentException
 	{
+		this.playerNum = playerNum; // Saves the number of players
+		
 		switch (playerNum)
 		{
 			case 0: break;
 			case 2: break;
 			case 4: break;
 			case 6: break;
-			default: throw new IllegalArgumentException(String.format(
+			default:throw new IllegalArgumentException(String.format(
 						"Invalid number of players (given %d, expected of {0:4, 6}.", playerNum));
 		}
 	}
@@ -49,14 +52,16 @@ public class Board
 	
 	public static boolean isSquareValid(int x, int y)
 	{
+		// Sanity check
 		if (x < 0 || x > 24) throw new IllegalArgumentException(String.format(
 				"Invalid x coordinate (given %d, expected of {0:24}.", x));
 		if (y < 0 || y > 16) throw new IllegalArgumentException(String.format(
 				"Invalid y coordinate (given %d, expected of {0:16}.", x));
 		
+		// Verifies if the coordinates are either both even or both odd
 		if ((x%2 == 0 && y%2 == 0) || (x%2 == 1 && y%2 == 1))
 		{
-			if (y > 8) y = 16 - y;
+			if (y > 8) y = 16 - y; // The board is symmetrical, so that saves time
 			switch (y)
 			{
 				case 0: return (x == 12);
@@ -68,7 +73,7 @@ public class Board
 				case 6:
 				case 7:
 				case 8: return (x > -5 + y && x < 30 - y);
-				default: throw new AssertionError();
+				default:throw new AssertionError(); // Should never happen
 			}
 		}
 		else return false;
